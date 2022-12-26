826462b3b3e416a8e16fd5d190aea3b46f7f1247
Veens <wingsfly2k19@gmail.com>
2022-12-26 21:36:04 +0530

    "to fetch the data based on users interest"

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,13 @@
-const Products = async (req, resp)=>{
-    resp.status(200).json({
-        id: 1,
-        productName: "Lenovo IdeaPad 3",
-        type: "Normal"
-    })
+const ProductModel = require('../models/ProductSchema');
 
-    // resp.send({
-    //     id: 1,
-    //     productName: "Lenovo IdeaPad 3",
-    //     type: "Normal"
-    // })
+const Products = async (req, resp)=>{
+    // to fetch data from mongodb atlas and view in our page
+    // const ProductsData = await ProductModel.find({});
+
+    // to fetch the data based on users interest
+    const ProductsData = await ProductModel.find(req.query);
+    console.log(ProductsData);
+    resp.status(200).json(ProductsData);
 }
 
 const ProductsTesting = async (req, resp) => {
