a6d9f5dc5140c316607058c7e0bac7f47fdcef18
Veens <wingsfly2k19@gmail.com>
2022-12-27 17:35:49 +0530

    "adding new data to json file"

--- routes/products.js
+++ routes/products.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const {Products, ProductsTesting} = require('../controllers/products');
 
 router.route('/products').get(Products);
-router.route('/products_testing').get(ProductsTesting);
+router.route('/products/testing').get(ProductsTesting);
 
 module.exports = router;
